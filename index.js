57585a7cef11f9b651fe3d0d8821e05b31f1119b
Kat Marchán <kzm@sykosomatic.org>
2017-04-09 01:35:21 -0700

    feat(agent): add ca/cert/key support to auto-agent (#15)
    Fixes: #14
    
    Incidentally, got rid of proxy-agent because the pac thing I'm sure
    literally no one would use (heh) was basically a MASSIVE dependency.
    
    BREAKING CHANGE: pac proxies are no longer supported.

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 let Cache
 const fetch = require('node-fetch')
-let ProxyAgent
+const LRU = require('lru-cache')
 const pkg = require('./package.json')
 const retry = require('promise-retry')
 let ssri
 const Stream = require('stream')
+const url = require('url')
 
 const RETRY_ERRORS = [
   'ECONNRESET', // remote socket closed on us
@@ -339,30 +340,99 @@ function remoteFetch (uri, opts) {
   })
 }
 
-let httpsAgent
-let httpAgent
+let AGENT_CACHE = new LRU({
+  max: 50
+})
+let HttpsAgent
+let HttpAgent
 function getAgent (uri, opts) {
+  const parsedUri = url.parse(uri)
+  const isHttps = parsedUri.protocol === 'https:'
+  const pxuri = getProxyUri(uri, opts)
+  const key = [
+    `https:${isHttps}`,
+    pxuri
+    ? `proxy:${pxuri.protocol}//${pxuri.host}:${pxuri.port}`
+    : '>no-proxy<',
+    `ca:${(isHttps && opts.ca) || '>no-ca<'}`,
+    `cert:${(isHttps && opts.cert) || '>no-cert<'}`,
+    `key:${(isHttps && opts.key) || '>no-key<'}`
+  ].join(':')
   if (opts.agent != null) {
     // `agent: false` has special behavior!
     return opts.agent
-  } else if (opts.proxy) {
-    if (!ProxyAgent) {
-      ProxyAgent = require('proxy-agent')
-    }
-    return new ProxyAgent(opts.proxy)
-  } else if (uri.trim().startsWith('https:')) {
-    if (!httpsAgent) {
-      const Agent = require('agentkeepalive').HttpsAgent
-      httpsAgent = new Agent({maxSockets: 15})
-    }
-    return httpsAgent
+  } else if (AGENT_CACHE.peek(key)) {
+    return AGENT_CACHE.get(key)
+  } else if (pxuri) {
+    const proxy = getProxy(pxuri, opts)
+    AGENT_CACHE.set(key, proxy)
+    return proxy
   } else {
-    if (!httpAgent) {
-      const Agent = require('agentkeepalive')
-      httpAgent = new Agent({maxSockets: 15})
+    if (isHttps && !HttpsAgent) {
+      HttpsAgent = require('agentkeepalive').HttpsAgent
+    } else if (!isHttps && !HttpAgent) {
+      HttpAgent = require('agentkeepalive')
     }
-    return httpAgent
+    const agent = isHttps
+    ? new HttpsAgent({
+      maxSockets: opts.maxSockets || 15,
+      ca: opts.ca,
+      cert: opts.cert,
+      key: opts.key
+    })
+    : new HttpAgent({
+      maxSockets: opts.maxSockets || 15
+    })
+    AGENT_CACHE.set(key, agent)
+    return agent
   }
+}
+
+function getProxyUri (uri, opts) {
+  const puri = url.parse(uri)
+  const proxy = opts.proxy || (
+    puri.protocol === 'https:' && process.env.https_proxy
+  ) || (
+    puri.protocol === 'http:' && (
+      process.env.https_proxy || process.env.http_proxy || process.env.proxy
+    )
+  )
+  return !checkNoProxy(uri) && (
+    typeof proxy === 'string'
+    ? url.parse(proxy)
+    : proxy
+  )
+}
+
+let HttpProxyAgent
+let HttpsProxyAgent
+let SocksProxyAgent
+function getProxy (proxyUrl, opts) {
+  let popts = {
+    host: proxyUrl.hostname,
+    port: proxyUrl.port,
+    protocol: proxyUrl.protocol,
+    path: proxyUrl.path,
+    ca: opts.ca,
+    cert: opts.cert,
+    key: opts.key,
+    maxSockets: opts.maxSockets || 15
+  }
+  if (proxyUrl.protocol === 'http:') {
+    if (!HttpProxyAgent) { HttpProxyAgent = require('http-proxy-agent') }
+    return new HttpProxyAgent(popts)
+  } else if (proxyUrl.protocol === 'https:') {
+    if (!HttpsProxyAgent) { HttpsProxyAgent = require('https-proxy-agent') }
+    return new HttpsProxyAgent(popts)
+  } else if (proxyUrl.startsWith('socks')) {
+    if (!SocksProxyAgent) { SocksProxyAgent = require('socks-proxy-agent') }
+    return new SocksProxyAgent(popts)
+  }
+}
+
+function checkNoProxy (uri) {
+  // TODO
+  return false
 }
 
 function isConditional (headers) {
